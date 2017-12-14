86f6fe020158b09da65c44bd251bb28edc4d3fdf
CruXial <benjamin@theones.dk>
2017-12-14 17:17:06 +0100

    Initial commit

---
+++ EzMessageAnnouncer.cs
@@ -0,0 +1,68 @@
+﻿using Rocket.Core.Plugins;
+using Rocket.Unturned;
+using Rocket.Unturned.Chat;
+using Rocket.Unturned.Player;
+using System;
+using System.Collections;
+using UnityEngine;
+
+using Logger = Rocket.Core.Logging.Logger;
+
+namespace Crux.EzMessageAnnouncer
+{
+    public class EzMessageAnnouncer : RocketPlugin<Configuration>
+    {
+        public static EzMessageAnnouncer Instance;
+        public static Configuration Config => Instance.Configuration.Instance;
+
+        protected override void Load()
+        {
+            Instance = this;
+
+            U.Events.OnPlayerConnected += Events_OnPlayerConnected;
+
+            Logger.Log("EzMessageAnnouncer Sucessfully Loaded!", color: ConsoleColor.Green);
+
+            loop();
+        }
+
+        protected override void Unload()
+        {
+            Logger.Log("EzMessageAnnouncer Sucessfully Unloaded!");
+
+            U.Events.OnPlayerConnected -= Events_OnPlayerConnected;
+        }
+
+        void Events_OnPlayerConnected(UnturnedPlayer player)
+        {
+            if (player != null)
+            {
+                StartCoroutine(StartDelayedUrlRequest(player));
+            }
+        }
+
+        private IEnumerator StartDelayedUrlRequest(UnturnedPlayer player)
+        {
+            yield return new WaitForSeconds(1.5f);
+        }
+
+        public static IEnumerator loop()
+        {
+            while (true)
+            {
+                UnturnedChat.Say(Config.Message1);
+
+                yield return new WaitForSeconds(Config.MessageInterval);
+                UnturnedChat.Say(Config.Message2);
+
+                yield return new WaitForSeconds(Config.MessageInterval);
+                UnturnedChat.Say(Config.Message3);
+
+                yield return new WaitForSeconds(Config.MessageInterval);
+                UnturnedChat.Say(Config.Message4);
+
+                yield return new WaitForSeconds(Config.MessageInterval);
+            }
+        }
+    }
+}
---
+++ EzMessageAnnouncerConfig.cs
@@ -0,0 +1,29 @@
+﻿using Rocket.API;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crux.EzMessageAnnouncer
+{
+    public class Configuration : IRocketPluginConfiguration
+    {
+        public string Message1;
+        public string Message2;
+        public string Message3;
+        public string Message4;
+
+        public float MessageInterval;
+
+        public void LoadDefaults()
+        {
+            Message1 = "Welcome to SERVER_NAME, Have a great stay ;)";
+            Message2 = "Be sure to join our discord.";
+            Message3 = "Please be respectful towards others.";
+            Message4 = "All communication remains in English.";
+
+            MessageInterval = 300;
+        }
+    }
+}
